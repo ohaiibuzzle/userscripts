f38111e11a77dcb30198a7b4f83e52b384a033c7
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2024-07-22 10:55:56 +0700

    fix (gd.js): prevent site from re-adding elements

--- gd.js
+++ gd.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Knock down the door made of glass.
 // @namespace    https://ohaiibuzzle.dev/
-// @version      v0.0.1
+// @version      v0.0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.glassdoor.com/*
@@ -10,13 +10,8 @@
 // ==/UserScript==
 
 (function() {
-    let hardOverlay = document.getElementById('Cont' + 'entW' + 'allH' + 'ardsell');
-    if (hardOverlay) {
-        hardOverlay.remove();
-    }
     document.getElementsByTagName("body")[0].style.overflow = "scroll";
     document.getElementsByTagName("body")[0].style.position = "unset";
-
     let style = document.createElement('style');
     style.innerHTML = `
     #LoginModal {
@@ -28,7 +23,19 @@
   `;
     document.head.appendChild(style);
 
-    window.addEventListener("scroll", function(event) {
-        event.stopPropagation();
-    }, true);
+    setInterval(function() {
+        let t1Overlay = document.getElementById('Cont' + 'entW' + 'allH' + 'ardsell');
+        if (t1Overlay) {
+            t1Overlay.hidden = true
+        }
+
+        let t2Overlay = document.getElementById('Har' + 'dsel' + 'lOver' + 'lay');
+        if (t2Overlay) {
+            t2Overlay.hidden = true;
+        }
+
+        window.addEventListener("scroll", function(event) {
+            event.stopPropagation();
+        }, true);
+    });
 })();
